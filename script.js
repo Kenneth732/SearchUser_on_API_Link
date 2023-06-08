f91186ae0da21b39a157b14c3baefd975f7cc1da
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-06-08 14:59:38 +0300

    css

--- script.js
+++ script.js
@@ -1,55 +1,65 @@
 document.querySelector('#form').addEventListener('submit', (e) => {
     e.preventDefault();
-
-    let searchInput = document.querySelector('#search');
-    let searchTerm = searchInput.value;
-
+  
+    const searchInput = document.querySelector('#search');
+    const searchTerm = searchInput.value;
+  
+    // Clear previous search results
     document.querySelector('#user-list').innerHTML = '';
     document.querySelector('#repos-list').innerHTML = '';
-
+  
+    // Perform user search
     fetch(`https://api.github.com/search/users?q=${searchTerm}`, {
-        headers:{
-            'Accept': 'application/vnd.github.v3+json'
-        }
-    }).then((res) => res.json())
-    .then((data) => {
-        const users = data.items;
-        users.map(user => {
-            displayUser(user);
-        })
-    }).catch(error => {
-        console.log('Error:', error)
+      headers: {
+        'Accept': 'application/vnd.github.v3+json'
+      }
+    })
+    .then(response => response.json())
+    .then(data => {
+      const users = data.items;
+      users.forEach(user => {
+        displayUser(user);
+      });
+    })
+    .catch(error => {
+      console.error('Error:', error);
     });
-
-    function displayUser(user){
-        const userList = document.querySelector('#user-list');
-        const li = document.createElement('li');
-        const link = document.createElement('a');
-        link.href = user.html_url;
-        link.target = '_blank';
-        link.textContent = user.login;
-        li.appendChild(link);
-        userList.appendChild(li);
-
-        li.addEventListener('click', () => {
-            fetch(`https://api.github.com/search/users/${user.login/repos}`, {
-                headers:{
-                    'Accept': 'application/vnd.github.v3+json'
-                }
-            })
-            .then((res) => res.json())
-            .then((data) => {
-                const repos = data;
-                displayRepos(repos)
-            }).catch(error => {
-                console.log('Error', error)
-            });
+  
+    // Display user details
+    function displayUser(user) {
+      const userList = document.querySelector('#user-list');
+      const li = document.createElement('li');
+      const link = document.createElement('a');
+      link.href = user.html_url;
+      link.target = '_blank';
+      link.textContent = user.login;
+      li.appendChild(link);
+      userList.appendChild(li);
+  
+      // Add event listener to retrieve user repositories when clicked
+      li.addEventListener('click', () => {
+        fetch(`https://api.github.com/users/${user.login}/repos`, {
+          headers: {
+            'Accept': 'application/vnd.github.v3+json'
+          }
+        })
+        .then(response => response.json())
+        .then(data => {
+          const repos = data;
+          displayRepos(repos);
+        })
+        .catch(error => {
+          console.error('Error:', error);
         });
+      });
     }
-
-    // Display userrepositorys
-
-    repos.forEach(repo => {
+  
+    // Display user repositories
+    function displayRepos(repos) {
+      const reposList = document.querySelector('#repos-list');
+      reposList.innerHTML = '';
+  
+      repos.forEach(repo => {
         const li = document.createElement('li');
         const link = document.createElement('a');
         link.href = repo.html_url;
@@ -58,4 +68,6 @@ document.querySelector('#form').addEventListener('submit', (e) => {
         li.appendChild(link);
         reposList.appendChild(li);
       });
-})
+    }
+  });
+  
