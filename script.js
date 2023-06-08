acf86b667b931cc749fbcf6ceee29369c8caf7c3
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-06-08 14:34:14 +0300

    use fetch to fetch data on api and handle error

--- script.js
+++ script.js
@@ -7,5 +7,19 @@ document.querySelector('#form').addEventListener('submit', (e) => {
     document.querySelector('#user-list').innerHTML = '';
     document.querySelector('#repos-list').innerHTML = '';
 
+    fetch(`https://api.github.com/search/users?q=${searchTerm}`, {
+        headers:{
+            'Accept': 'application/vnd.github.v3+json'
+        }
+    }).then((res) => res.json())
+    .then((data) => {
+        const users = data.items;
+        users.map(user => {
+            displayUser(user);
+        })
+    }).catch(error => {
+        console.log('Error:', error)
+    });
 
+    function displayUser(user){}
 })
