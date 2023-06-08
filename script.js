518c9a04455ed98a686eafbcc3dddeb620aa541f
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-06-08 14:33:31 +0300

    declare searIn... and search plus clear inputs

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 document.querySelector('#form').addEventListener('submit', (e) => {
     e.preventDefault();
 
-    let 
+    let searchInput = document.querySelector('#search');
+    let searchTerm = searchInput.value;
+
+    document.querySelector('#user-list').innerHTML = '';
+    document.querySelector('#repos-list').innerHTML = '';
+
+
 })
