28c79a84ea374a0c46ccb4732cfc55d313536a71
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-06-08 15:11:55 +0300

    readme

--- README.md
+++ README.md
@@ -1,1 +1,75 @@
 # BlackCoder@CodeWise.com
+
+
+
+
+> 1. The code starts by selecting the form element with the ID "form" and adding an event listener for the "submit" event. This event will be triggered when the form is submitted.
+```javascript
+document.querySelector('#form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    // Code to execute when the form is submitted
+});
+```
+
+> 2. Inside the event listener function, the `e.preventDefault()` statement is used to prevent the default form submission behavior, which would cause a page refresh.
+
+> 3. The code then selects the input element with the ID "search" and retrieves the value entered by the user.
+```javascript
+const searchInput = document.querySelector('#search');
+const searchTerm = searchInput.value;
+```
+
+> 4. Next, the previous search results are cleared by setting the `innerHTML` of the user list and repository list elements to an empty string.
+```javascript
+document.querySelector('#user-list').innerHTML = '';
+document.querySelector('#repos-list').innerHTML = '';
+```
+
+> 5. The code performs a user search by making a fetch request to the GitHub API's user search endpoint, passing the search term as a query parameter.
+```javascript
+fetch(`https://api.github.com/search/users?q=${searchTerm}`, {
+    headers: {
+        'Accept': 'application/vnd.github.v3+json'
+    }
+})
+.then(response => response.json())
+.then(data => {
+    // Code to execute when the response is received
+})
+.catch(error => {
+    console.error('Error:', error);
+});
+```
+
+> 6. The response from the API is converted to JSON using the `response.json()` method. Then, the data is accessed and stored in the `users` variable.
+
+> 7. The code loops through each user in the `users` array and calls the `displayUser` function to display the user details.
+```javascript
+users.forEach(user => {
+    displayUser(user);
+});
+```
+
+> 8. The `displayUser` function is defined to create a list item element (`li`) and an anchor element (`a`) for each user. The user's GitHub profile link is set as the anchor's `href`, and their username is set as the anchor's text content. The list item is appended to the user list element (`#user-list`).
+```javascript
+function displayUser(user) {
+    // Code to create and append the user element
+}
+```
+
+> 9. An event listener is added to each user list item (`li`) to retrieve the user's repositories when it is clicked. The event listener makes a fetch request to the GitHub API's user repositories endpoint, passing the user's login (username) as a parameter.
+```javascript
+li.addEventListener('click', () => {
+    // Code to retrieve and display user repositories
+});
+```
+
+> 10. Inside the event listener, the response from the API is converted to JSON, and the data is accessed and stored in the `repos` variable.
+
+> 11. The `displayRepos` function is defined to create a list item element (`li`) and an anchor element (`a`) for each repository. The repository's GitHub link is set as the anchor's `href`, and the repository name is set as the anchor's text content. The list item is appended to the repository list element (`#repos-list`).
+```javascript
+function displayRepos(repos) {
+    // Code to create and append the repository elements
+}
+```
+
--- script.js
+++ script.js
@@ -1,73 +1,72 @@
 document.querySelector('#form').addEventListener('submit', (e) => {
     e.preventDefault();
-  
+
     const searchInput = document.querySelector('#search');
     const searchTerm = searchInput.value;
-  
+
     // Clear previous search results
     document.querySelector('#user-list').innerHTML = '';
     document.querySelector('#repos-list').innerHTML = '';
-  
+
     // Perform user search
     fetch(`https://api.github.com/search/users?q=${searchTerm}`, {
-      headers: {
-        'Accept': 'application/vnd.github.v3+json'
-      }
-    })
-    .then(response => response.json())
-    .then(data => {
-      const users = data.items;
-      users.forEach(user => {
-        displayUser(user);
-      });
-    })
-    .catch(error => {
-      console.error('Error:', error);
-    });
-  
-    // Display user details
-    function displayUser(user) {
-      const userList = document.querySelector('#user-list');
-      const li = document.createElement('li');
-      const link = document.createElement('a');
-      link.href = user.html_url;
-      link.target = '_blank';
-      link.textContent = user.login;
-      li.appendChild(link);
-      userList.appendChild(li);
-  
-      // Add event listener to retrieve user repositories when clicked
-      li.addEventListener('click', () => {
-        fetch(`https://api.github.com/users/${user.login}/repos`, {
-          headers: {
+        headers: {
             'Accept': 'application/vnd.github.v3+json'
-          }
-        })
+        }
+    })
         .then(response => response.json())
         .then(data => {
-          const repos = data;
-          displayRepos(repos);
+            const users = data.items;
+            users.forEach(user => {
+                displayUser(user);
+            });
         })
         .catch(error => {
-          console.error('Error:', error);
+            console.error('Error:', error);
         });
-      });
-    }
-  
-    // Display user repositories
-    function displayRepos(repos) {
-      const reposList = document.querySelector('#repos-list');
-      reposList.innerHTML = '';
-  
-      repos.forEach(repo => {
+
+    // Display user details
+    function displayUser(user) {
+        const userList = document.querySelector('#user-list');
         const li = document.createElement('li');
         const link = document.createElement('a');
-        link.href = repo.html_url;
+        link.href = user.html_url;
         link.target = '_blank';
-        link.textContent = repo.name;
+        link.textContent = user.login;
         li.appendChild(link);
-        reposList.appendChild(li);
-      });
+        userList.appendChild(li);
+
+        // Add event listener to retrieve user repositories when clicked
+        li.addEventListener('click', () => {
+            fetch(`https://api.github.com/users/${user.login}/repos`, {
+                headers: {
+                    'Accept': 'application/vnd.github.v3+json'
+                }
+            })
+                .then(response => response.json())
+                .then(data => {
+                    const repos = data;
+                    displayRepos(repos);
+                })
+                .catch(error => {
+                    console.error('Error:', error);
+                });
+        });
     }
-  });
-  
+
+    // Display user repositories
+    function displayRepos(repos) {
+        const reposList = document.querySelector('#repos-list');
+        reposList.innerHTML = '';
+
+        repos.forEach(repo => {
+            const li = document.createElement('li');
+            const link = document.createElement('a');
+            link.href = repo.html_url;
+            link.target = '_blank';
+            link.textContent = repo.name;
+            li.appendChild(link);
+            reposList.appendChild(li);
+        });
+    }
+});
